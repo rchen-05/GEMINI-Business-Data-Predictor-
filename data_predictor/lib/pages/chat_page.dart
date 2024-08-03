25650ba7d4ca07594077ec87e8558a7cdeadd7e8
danielribeiro12 <98743528+danielribeiro12@users.noreply.github.com>
2024-08-04 00:56:23 +0100

    ui perfected

--- data_predictor/lib/pages/chat_page.dart
+++ data_predictor/lib/pages/chat_page.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:js_interop';
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
@@ -10,10 +12,13 @@ import 'package:firebase_storage/firebase_storage.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
+  
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
+
+
 
 class _ChatPageState extends State<ChatPage> {
   List<ChatMessage> _messages = <ChatMessage>[];
@@ -21,15 +26,13 @@ class _ChatPageState extends State<ChatPage> {
   final ChatUser _currentUser = ChatUser(id: '1', firstName: 'Daniel', lastName: 'Bobby');
   final ChatUser _geminiUser = ChatUser(id: '2', firstName: 'Ai', lastName: 'man');
 
+  List<ChatUser> _typingUsers = <ChatUser>[];
   final TextEditingController _textController = TextEditingController();
+  
 
   Future<void> getChatResponse(ChatMessage message) async {
     final userMessage = message.text;
     final url = 'http://127.0.0.1:5001/chat'; // Flask server URL
-
-    setState(() {
-      _messages.insert(0, message);
-    });
 
     try {
       final response = await http.post(
@@ -85,6 +88,9 @@ class _ChatPageState extends State<ChatPage> {
           ));
       });
     }
+    setState(() {
+      _typingUsers.remove(_geminiUser);
+    });
   }
 
   Future<void> pickFile() async {
@@ -127,17 +133,48 @@ class _ChatPageState extends State<ChatPage> {
       ),
       body: DashChat(
         currentUser: _currentUser,
+        typingUsers: _typingUsers,
         inputOptions: InputOptions(
           sendOnEnter: true,
           alwaysShowSend: true,
           trailing: inputOptions()
         ),
-        messageOptions: const MessageOptions(
+        messageOptions:  MessageOptions(
           currentUserContainerColor: Colors.black,
           containerColor: Color.fromARGB(255, 184, 60, 22),
           textColor: Colors.white,
-        ),
+          messageTextBuilder: (currentMessage, previousMessage, nextMessage) {
+             if (currentMessage.user.id == _geminiUser.id && nextMessage == null) {
+              return AnimatedTextKit(
+                animatedTexts: [
+                  TyperAnimatedText(
+                    currentMessage.text,
+                    speed: const Duration(milliseconds: 30),
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+                totalRepeatCount: 1,
+              );
+            } else {
+              return SelectableText(
+                currentMessage.text,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              );
+            }
+          },
+  ),
         onSend: (ChatMessage message) {
+          setState(() {
+            _messages.insert(0, message);
+            _typingUsers.add(_geminiUser);
+          });
+          
           getChatResponse(message);
         },
         messages: _messages,
